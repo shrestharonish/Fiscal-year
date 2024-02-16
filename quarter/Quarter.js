5add9045c3c11dc70af91f7ee305a3b740f91bc3
Ronish Shrestha <shrestha.ronish500@gmail.com>
2024-02-17 00:06:54 +0545

    finally complete

--- quarter/Quarter.js
+++ quarter/Quarter.js
@@ -1,48 +1,50 @@
-function getActualQuarter() {
-  const Quarters = [
-    {
-      quarterName: "Q1",
-      quartersByMonth: ["Shrawan", "Bhadra", "Ashoj"],
-      quartersByCount: [4, 5, 6],
-    },
-    {
-      quarterName: "Q2",
-      quartersByMonth: ["Kartik", "Mangsir", "Poush"],
-      quartersByCount: [7, 8, 9],
-    },
-    {
-      quarterName: "Q3",
-      quartersByMonth: ["Magh", "Falgun", "Chaitra"],
-      quartersByCount: [10, 11, 12],
-    },
-    {
-      quarterName: "Q4",
-      quartersByMonth: ["Baisakh", "Jestha", "Asadh"],
-      quartersByCount: [1, 2, 3],
-    },
-  ];
-  return Quarters;
-}
+//  This is the actual quarter in a year
+// function getActualQuarter() {
+//   const Quarters = [
+//     {
+//       quarterName: "Q1",
+//       quartersByMonth: ["Shrawan", "Bhadra", "Ashoj"],
+//       quartersByCount: [4, 5, 6],
+//     },
+//     {
+//       quarterName: "Q2",
+//       quartersByMonth: ["Kartik", "Mangsir", "Poush"],
+//       quartersByCount: [7, 8, 9],
+//     },
+//     {
+//       quarterName: "Q3",
+//       quartersByMonth: ["Magh", "Falgun", "Chaitra"],
+//       quartersByCount: [10, 11, 12],
+//     },
+//     {
+//       quarterName: "Q4",
+//       quartersByMonth: ["Baisakh", "Jestha", "Asadh"],
+//       quartersByCount: [1, 2, 3],
+//     },
+//   ];
+//   return Quarters;
+// }
 
+// This is the required custom quarter in a year
 function getCustomQuarter() {
   const Quarters = [
     {
       quarterName: "Q1",
-      quartersByMonth: ["Shrawan", "Bhadra", "Ashoj", "kartik"],
+      quartersByMonth: ["Shrawan - kartik"],
       quartersByCount: [4, 5, 6, 7],
     },
     {
       quarterName: "Q2",
-      quartersByMonth: ["Mangsir", "Poush", "Magh", "Falgun"],
+      quartersByMonth: ["Mangsir - Falgun"],
       quartersByCount: [8, 9, 10, 11],
     },
     {
       quarterName: "Q3",
-      quartersByMonth: ["Chaitra", "Baisakh", "Jestha", "Asadh"],
+      quartersByMonth: ["Chaitra - Asadh"],
       quartersByCount: [12, 1, 2, 3],
     },
   ];
   return Quarters;
 }
 
-module.exports = { getActualQuarter, getCustomQuarter };
+module.exports = { getCustomQuarter };
